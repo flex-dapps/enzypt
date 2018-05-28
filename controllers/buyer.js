315a451b11f5ec22d6f2a33d09da63b5a27a13bf
Tom Nash <contacttomnash@gmail.com>
2018-05-28 10:21:03 +1000

    Finish up buyer & seller API endpoints

--- controllers/buyer.js
+++ controllers/buyer.js
@@ -5,90 +5,127 @@ const config = require('../config')
 const web3 = require('../services/web3')
 
 class BuyerController {
-
-  static bind (app) {
+  static bind(app) {
     app.get('/:urlSlug', this.getBuy)
     app.post('/rand', jsonParser, this.postRand)
     app.post('/msg', jsonParser, this.postMsg)
     app.post('/buy', jsonParser, this.postBuy)
   }
 
-  static async getBuy (req, res, next) {
+  static async getBuy(req, res, next) {
     if (!req.params.urlSlug) return res.status(400).send()
-    const doc = await req.db.collection('sales').findOne({
-      urlSlug: req.params.urlSlug
-    }, {
-      metaFileHash: 1,
-      _id: 0
-    })
-    res.send(doc.metaFileHash)
+    try {
+      const doc = await req.db.collection('sales').findOne(
+        {
+          urlSlug: req.params.urlSlug
+        },
+        {
+          metaFileHash: 1,
+          iv: 1,
+          downloads: 1,
+          _id: 0
+        }
+      )
+      res.send(doc)
+    } catch (e) {
+      return res.status(404).send()
+    }
   }
 
-  static async postRand (req, res, next) {
+  static async postRand(req, res, next) {
     const { publicKey } = req.body
-    if (!publicKey || !web3.utils.isAddress(publicKey)) return res.status(400).send()
+    if (!publicKey || !web3.utils.isAddress(publicKey))
+      return res.status(400).send()
     const lowerCasePublicKey = publicKey.toLowerCase()
     const randomString = randomstring.generate(16)
-    await req.db.collection('auth').findOneAndUpdate({
-      publicKey: lowerCasePublicKey
-    }, {
-      $set: {
-        randomString: randomString
+    await req.db.collection('auth').findOneAndUpdate(
+      {
+        publicKey: lowerCasePublicKey
+      },
+      {
+        $set: {
+          randomString: randomString
+        }
+      },
+      {
+        upsert: true
       }
-    }, {
-      upsert: true 
-    })
+    )
     res.send(randomString)
   }
 
-  static async postMsg (req, res, next) {
+  static async postMsg(req, res, next) {
     const { publicKey, signedMessage, urlSlug } = req.body
     if (!publicKey || !signedMessage) return res.status(400).send()
     const lowerCasePublicKey = publicKey.toLowerCase()
-    const doc = await req.db.collection('auth').findOne({ publicKey: lowerCasePublicKey })
-    const messageToRecover = config.MESSAGE_TO_SIGN.replace('%', lowerCasePublicKey).replace('%', doc.randomString)
-    const recoveredKey = await web3.eth.accounts.recover(messageToRecover, signedMessage, false)
+    const doc = await req.db
+      .collection('auth')
+      .findOne({ publicKey: lowerCasePublicKey })
+    const messageToRecover = config.MESSAGE_TO_SIGN.replace(
+      '%',
+      lowerCasePublicKey
+    ).replace('%', doc.randomString)
+    const recoveredKey = await web3.eth.accounts.recover(
+      messageToRecover,
+      signedMessage,
+      false
+    )
     if (recoveredKey.toLowerCase() === publicKey.toLowerCase()) {
       const randomHex = web3.utils.randomHex(32).toUpperCase()
-      req.db.collection('purchases').insertOne({
-        fromAddress: recoveredKey,
-        purchaseRef: randomHex,
-        urlSlug: urlSlug
-      }, () => {
-        res.send(randomHex)
-      })
+      req.db.collection('purchases').insertOne(
+        {
+          fromAddress: recoveredKey.toLowerCase(),
+          purchaseRef: randomHex,
+          urlSlug: urlSlug
+        },
+        () => {
+          res.send(randomHex)
+        }
+      )
     } else {
       res.status(418).send()
     }
   }
 
-  static postBuy (req, res, next) {
+  static async postBuy(req, res, next) {
     const { txHash } = req.body
     if (!txHash) return res.status(400).send()
-    Etherscan.pollStatus(txHash, async (e, successful) => {
-      if (e || !successful) return res.status(418).send()
-      const receipt = await web3.eth.getTransaction(txHash)
-      if (!receipt || !receipt.input || !receipt.from) return res.status(418).send()
-      const fromAddress = receipt.from.toLowerCase()
-      const inputData = receipt.input.toUpperCase()
-      const purchaseDoc = await req.db.collection('purchases').findOneAndUpdate({
-        fromAddress: fromAddress,
-        purchaseRef: inputData
-      }, {
-        $set: {
-          paid: true
-        }
-      })
-      if (!purchaseDoc || !purchaseDoc.value) return res.status(418).send()
-      const fileDoc = await req.db.collection('sales').findOne({
-        urlSlug: purchaseDoc.value.urlSlug
-      })
-      if (!fileDoc) return res.status(418).send()
-      res.send(fileDoc.zipFileHash)
-    })
+    const tx = await web3.eth.getTransaction(txHash)
+    if (!tx || !tx.input || !tx.from || !tx.value) return res.status(418).send()
+    const fromAddress = tx.from.toLowerCase()
+    const inputData = tx.input.toUpperCase()
+    const ethSent = web3.utils.fromWei(tx.value, 'ether')
+
+    const purchaseDoc = await req.db
+      .collection('purchases')
+      .findOne({ fromAddress: fromAddress, purchaseRef: inputData })
+
+    const fileDoc = await req.db
+      .collection('sales')
+      .findOne({ urlSlug: purchaseDoc.urlSlug })
+
+    if (!purchaseDoc) return res.status(418).send()
+    if (!fileDoc) return res.status(418).send()
+
+    if (ethSent < Number(fileDoc.ethPrice)) return res.status(418).send()
+
+    if (!purchaseDoc.paid) {
+      await req.db
+        .collection('purchases')
+        .findOneAndUpdate(
+          { fromAddress: fromAddress, purchaseRef: inputData },
+          { $set: { paid: true } }
+        )
+      await req.db
+        .collection('sales')
+        .findOneAndUpdate(
+          { urlSlug: purchaseDoc.urlSlug },
+          { $inc: { downloads: 1 } }
+        )
+    }
+
+    res.send(fileDoc.zipFileHash)
   }
-
-
 }
 
 module.exports = BuyerController
--- controllers/seller.js
+++ controllers/seller.js
@@ -2,24 +2,29 @@ const randomstring = require('randomstring')
 const jsonParser = require('body-parser').json()
 
 class SellerController {
-
-  static bind (app) {
+  static bind(app) {
     app.post('/sell', jsonParser, this.postSell)
   }
 
-  static postSell (req, res, next) {
-    const { zipFileHash, metaFileHash } = req.body
-    if (!zipFileHash || !metaFileHash) return res.status(400).end()
+  static postSell(req, res, next) {
+    const { zipFileHash, metaFileHash, iv, ethPrice } = req.body
+    console.log(req.body)
+    if (!zipFileHash || !metaFileHash || !iv) return res.status(400).end()
     const urlSlug = randomstring.generate()
-    req.db.collection('sales').insertOne({
-      zipFileHash,
-      metaFileHash,
-      urlSlug
-    }, doc => {
-      res.send(urlSlug)
-    })
+    req.db.collection('sales').insertOne(
+      {
+        zipFileHash,
+        metaFileHash,
+        iv,
+        ethPrice,
+        downloads: 0,
+        urlSlug
+      },
+      doc => {
+        res.send(urlSlug)
+      }
+    )
   }
-
 }
 
 module.exports = SellerController
