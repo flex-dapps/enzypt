328689a8b2ffb0160b877f4c30704c90125934e7
Tom Nash <contacttomnash@gmail.com>
2018-07-25 14:24:49 +1000

    Remove network log

--- services/web3.js
+++ services/web3.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const Web3 = require('web3')
 const config = require('../config')
 
-console.log(process.env.NETWORK)
-
 const web3 = new Web3(config.ETH_NODE_RPC_URL[process.env.NETWORK])
 
 module.exports = web3
